de2f49204f71b31b2a4f43c771ceab782022c907
Simon Schwichtenberg <schwicht@mail.upb.de>
2018-08-10 17:12:12 +0200

    Update

--- viatra/test/ViatraBenchmark.java
+++ viatra/test/ViatraBenchmark.java
@@ -1,17 +1,25 @@
+import static org.junit.Assert.assertTrue;
+
+import java.lang.annotation.Repeatable;
 import java.util.Collection;
 import java.util.Map;
 import java.util.function.Function;
 
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.viatra.query.runtime.api.ViatraQueryEngine;
 import org.eclipse.viatra.query.runtime.base.api.BaseIndexOptions;
 import org.eclipse.viatra.query.runtime.emf.EMFScope;
 import org.eclipse.viatra.query.runtime.exception.ViatraQueryException;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -28,95 +36,157 @@ import viatra.ThrownExceptions;
 
 public class ViatraBenchmark {
 
-	private static Model model;
-	private static int repetitions = 100;
 	
-    private static long initialUsedMemory = 0;
-    private static long finalUsedMemory = 0;
+    private long initialUsedMemory = 0;
+    private long finalUsedMemory = 0;
+    private long beginTime = 0;
+    private long endTime = 0;
+    private ResourceSet resourceSet;
+    protected Resource resource = null;
 	
-	protected static ViatraQueryEngine engine = null;
+	protected ViatraQueryEngine engine = null;
+	protected BaseIndexOptions options = null;
+	protected EMFScope scope = null;
+	
 	
 	@BeforeClass
-	public static void setUp() throws Exception {
+	public static void beforeClass(){
 		
-		//Thread.sleep(3000);
 		
-		//new EMFPatternLanguageStandaloneSetup().createInjectorAndDoEMFRegistration();
+		
+		ViatraBenchmark vb = new ViatraBenchmark();
+		try {
+			vb.setUp();
+			vb.emptyTextElementInJavadoc();
+			vb.tearDown();
+			
+			vb.setUp();
+			vb.grabats09();
+			vb.tearDown();
+			
+			vb.setUp();
+			vb.invisibleMethods();
+			vb.tearDown();
+			
+			vb.setUp();
+			vb.thrownExceptions();
+			vb.tearDown();
+			
+			vb.setUp();
+			vb.textElementInJavadoc();
+			vb.tearDown();
+			vb = null;
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Before
+	public void setUp() throws Exception {
 		
 		Java_PackageImpl.init();
 		
-		Function<TypeAccess, Boolean> kk = x->true;
-		
-		kk.apply(null);
-		
-		
-        Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
-        Map<String, Object> m = reg.getExtensionToFactoryMap();
-        m.put("xmi", new XMIResourceFactoryImpl());
-		
         // Obtain a new resource set
-        ResourceSet resSet = new ResourceSetImpl();
+        resourceSet = new ResourceSetImpl();
         
-        resSet.getPackageRegistry().put("java_", Java_PackageImpl.eINSTANCE);
+        resourceSet.getPackageRegistry().put("java_", Java_PackageImpl.eINSTANCE);
+        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
         
-        //Resource resource = resSet.getResource(URI.createURI("model/org.eclipse.gmt.modisco.java.kyanos.xmi"), true);
-        Resource resource = resSet.getResource(URI.createURI("model/org.eclipse.jdt.core.xmi"), true);
-        	
-        BaseIndexOptions options = new BaseIndexOptions().withDanglingFreeAssumption(false);
-        EMFScope scope = new EMFScope(resSet, options);
+        resource = resourceSet.getResource(URI.createURI("model/org.eclipse.gmt.modisco.java.kyanos.xmi"), true);
+        
+        //resource = resourceSet.getResource(URI.createURI("model/org.eclipse.jdt.core.xmi"), true);
+        
+
+        
+        
+        options = new BaseIndexOptions().withDanglingFreeAssumption(false);
+        scope = new EMFScope(resourceSet, options);
         //engine = AdvancedViatraQueryEngine.createUnmanagedEngine(scope);
         engine = ViatraQueryEngine.on(scope);
         
-        model = (Model) resource.getContents().get(0);
-        
-        Runtime.getRuntime().gc();
-    	initialUsedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+	}
 	
+	protected void startWatch() {
+		beginTime = System.currentTimeMillis();
 		
 	}
-	@AfterClass
-	public static void afterClass() throws Exception {
+	
+	protected void stopWatch() {
+		endTime = System.currentTimeMillis();
+		System.out.println(endTime - beginTime);
+		
 		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		finalUsedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-		System.out.println("initialUsedMemory" +initialUsedMemory);
-		System.out.println("finalUsedMemory" +finalUsedMemory);
+		System.out.println("used memory: "+(finalUsedMemory/(1024*1024)));
+		
+		
 	}
+	
+	@After
+    public void tearDown() throws Exception {
+        
+		this.resource = null;
+		
+		this.resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().clear();
+		this.resourceSet.getPackageRegistry().clear();
+		this.resourceSet = null;
+		
+		this.engine = null;
+		
+    }
+
+	
+
 
 	@Test
-	@Ignore
-	public void grabats09Query() {
+	public void grabats09() {
+		System.out.println("grabats09");
 		
-			
+		startWatch();
+		Collection<viatra.Grabats09.Match> matches=null;
 		try {
-			long time = System.currentTimeMillis();
 			
-			Collection<viatra.Grabats09.Match> matches = Grabats09.instance().getMatcher(engine).getAllMatches();
-			time = System.currentTimeMillis()-time;
-			System.out.println("viatra: grabats09: "+time);
+			matches = Grabats09.instance().getMatcher(engine).getAllMatches();
 //			System.out.println("Result set size : " + matches.size());
+			
+			
 		} catch (ViatraQueryException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		stopWatch();
+		
 		
 	}
 	
 	@Test
-	@Ignore
 	public void invisibleMethods() {
+		System.out.println("invisibleMethods");
 		
+		startWatch();
 		try {
-			long time = System.currentTimeMillis();
 			Collection<viatra.InvisibleMethods.Match> matches = InvisibleMethods.instance().getMatcher(engine).getAllMatches();
 			
-			time = System.currentTimeMillis()-time;
-			System.out.println("viatra: invisibleMethods: "+time);
-//			System.out.println("Result set size : " + matches.size());
 		} catch (ViatraQueryException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		stopWatch();
 		
 
 		
@@ -124,59 +194,49 @@ public class ViatraBenchmark {
 	
 	
 	@Test
-	@Ignore
 	public void thrownExceptions() {
-		
+		System.out.println("thrownExceptions");
+		startWatch();
 		try {
-			long time = System.currentTimeMillis();
 			Collection<viatra.ThrownExceptions.Match> matches = ThrownExceptions.instance().getMatcher(engine).getAllMatches();
 			
 			
-			time = System.currentTimeMillis()-time;
-//			System.out.println("Result set size : " + matches.size());
-			System.out.println("viatra: thrownExceptions: "+time);
 		} catch (ViatraQueryException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		stopWatch();
 		
 	}
 	
 	@Test
 	public void textElementInJavadoc() {
-		
+		System.out.println("textElementInJavadoc");
+		startWatch();
 		try {
-			long time = System.currentTimeMillis();
 			Collection<viatra.TextElementInJavadoc.Match> matches = TextElementInJavadoc.instance().getMatcher(engine).getAllMatches();
-//			System.out.println("Result set size : " + matches.size());
-			
-			time = System.currentTimeMillis()-time;
-			System.out.println("viatra: textElementInJavadoc: "+time);
 		} catch (ViatraQueryException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		stopWatch();
 
 		
 	}
 	
 	@Test
-	@Ignore
 	public void emptyTextElementInJavadoc() {
-		
+		System.out.println("emptyTextElementInJavadoc");
+		startWatch();
 		try {
-			long time = System.currentTimeMillis();
 			Collection<viatra.EmptyTextElementInJavadoc.Match> matches = EmptyTextElementInJavadoc.instance().getMatcher(engine).getAllMatches();
 			
-			time = System.currentTimeMillis()-time;
-//			System.out.println("Result set size : " + matches.size());
-			System.out.println("viatra: emptyTextElementInJavadoc: "+time);
 			
 		} catch (ViatraQueryException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		stopWatch();
 
 
 		
