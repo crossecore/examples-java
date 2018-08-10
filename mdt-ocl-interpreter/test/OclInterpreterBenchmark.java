56bebb4a1d94b5f66323d75b6568a139678d755d
Simon Schwichtenberg <schwicht@mail.upb.de>
2018-08-11 00:48:04 +0200

    OCL 6.5.0

--- mdt-ocl-interpreter/test/OclInterpreterBenchmark.java
+++ mdt-ocl-interpreter/test/OclInterpreterBenchmark.java
@@ -1,12 +1,11 @@
 
-import java.util.List;
-import java.util.Map;
+import java.util.Collection;
+import java.util.function.Function;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -17,88 +16,166 @@ import org.eclipse.ocl.Query;
 import org.eclipse.ocl.ecore.EcoreEnvironmentFactory;
 import org.eclipse.ocl.expressions.OCLExpression;
 import org.eclipse.ocl.helper.OCLHelper;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java_.FieldDeclaration;
-import java_.Java_Factory;
-import java_.Java_Package;
-import java_.Model;
-import java_.impl.Java_FactoryImpl;
+import java_.TypeAccess;
 import java_.impl.Java_PackageImpl;
 
 
 
 public class OclInterpreterBenchmark {
 
-	
     protected OCL ocl = null;
     protected OCLHelper oclHelper = null;
     
     protected OCLExpression<EClassifier> expression = null;
     protected Query<EClassifier, EClass, EObject> query = null;
     
+    private long initialUsedMemory = 0;
+    private long finalUsedMemory = 0;
+    private long beginTime = 0;
+    private long endTime = 0;
+    
+    
+    protected ResourceSet resourceSet = null;
     protected Resource resource = null;
+    
+	//@BeforeClass
+	public static void beforeClass(){
+		
+		
+		OclInterpreterBenchmark vb = new OclInterpreterBenchmark();
+		try {
+			vb.setUp();
+			vb.emptyTextElementInJavadoc();
+			vb.tearDown();
+			
+			vb.setUp();
+			vb.grabats09();
+			vb.tearDown();
+			
+			vb.setUp();
+			vb.invisibleMethods();
+			vb.tearDown();
+			
+			vb.setUp();
+			vb.thrownExceptions();
+			vb.tearDown();
+			
+			vb.setUp();
+			vb.textElementInJavadoc();
+			vb.tearDown();
+			vb = null;
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+    
+    
+    
+	protected void startWatch() {
+		beginTime = System.currentTimeMillis();
+		
+	}
+	
+	protected void stopWatch() {
+		endTime = System.currentTimeMillis();
+		System.out.println(endTime - beginTime);
+		
+		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		finalUsedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+		System.out.println("used memory: "+(finalUsedMemory/(1024*1024)));
+		
+	}
 	
 	@Before
 	public void setUp() throws Exception {
 		
 		Java_PackageImpl.init();
 		
-		
-        Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
-        Map<String, Object> m = reg.getExtensionToFactoryMap();
-        m.put("xmi", new XMIResourceFactoryImpl());
-		
-        // Obtain a new resource set
-        ResourceSet resSet = new ResourceSetImpl();
+        resourceSet = new ResourceSetImpl();
         
-        resSet.getPackageRegistry().put("java_", Java_PackageImpl.eINSTANCE);
+        resourceSet.getPackageRegistry().put("java_", Java_PackageImpl.eINSTANCE);
+        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
         
-//        FieldDeclaration fielddeclaration = Java_Factory.eINSTANCE.createFieldDeclaration();
-//        EStructuralFeature fragments = fielddeclaration.eClass().getEStructuralFeature("fragments");
-//        Object value = fielddeclaration.eGet(fragments, true);
+        resource = resourceSet.getResource(URI.createURI("model/org.eclipse.gmt.modisco.java.kyanos.xmi"), true);
         
-		//EClass x = Java_PackageImpl.eINSTANCE.getFieldDeclaration();
-//		EStructuralFeature fragments = x.getEStructuralFeature("fragments");
-//		Object value = x.eGet(fragments, true);
-//		List<EStructuralFeature> features = x.getEStructuralFeatures();
-//		List<EStructuralFeature> allfeatures = x.getEAllStructuralFeatures();
+        //resource = resourceSet.getResource(URI.createURI("model/org.eclipse.jdt.core.xmi"), true);
         
-        resource = resSet.getResource(URI.createURI("model/org.eclipse.gmt.modisco.java.kyanos.xmi"), true);
+        
+        ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
+        oclHelper = ocl.createOCLHelper();
+        
         	
 	}
 	
-
-	
+	//@After
+    public void tearDown() throws Exception {
+        
+		
+		this.resource = null;
+		
+		this.resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().clear();
+		this.resourceSet.getPackageRegistry().clear();
+		this.resourceSet = null;
+		
+		
+        this.oclHelper = null;
+        this.ocl = null;
+        this.expression = null;
+        this.query = null;
+        
+        
+    }
 	
 	@Test
-	public void throwsExceptions(){
-		long time = System.currentTimeMillis();
+	public void thrownExceptions(){
+		System.out.println("thrownExceptions");
+		startWatch();
 		
-        this.ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
-        this.oclHelper = ocl.createOCLHelper();
-        
         oclHelper.setContext(Java_PackageImpl.eINSTANCE.getModel());
         try {
-            expression = oclHelper.createQuery("ClassDeclaration.allInstances()->collect(bodyDeclarations)->select(each | each.oclIsTypeOf(MethodDeclaration))->select(each | (not each.modifier.oclIsUndefined()) and (not each.modifier.visibility.oclIsUndefined()) and (each.modifier.visibility = VisibilityKind::private or each.modifier.visibility = VisibilityKind::protected))->asSequence()");
+            expression = oclHelper.createQuery("ClassDeclaration.allInstances()->"
+                    + "collect(bodyDeclarations)->"
+                    + "select(each | each.oclIsTypeOf(MethodDeclaration))->"
+                    + "collect(each | each.oclAsType(MethodDeclaration).thrownExceptions)"
+                    + "->asSequence()");
         } catch (ParserException e) {
             e.printStackTrace();
         }
-        this.query = ocl.createQuery(expression);
+        query = ocl.createQuery(expression);
         Object res = query.evaluate(resource.getContents().get(0));
-        time = System.currentTimeMillis()-time;
-        System.out.println("mdt-interpreter: throwsExceptions: "+time);
+
+        stopWatch();
+        System.out.println("results: "+((Collection)res).size());
 
 	}
-		
+
+
+			
 	@Test
 	public void invisibleMethods(){
-		long time = System.currentTimeMillis();
+		System.out.println("invisibleMethods");
+		startWatch();
 		
-        this.ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
-        this.oclHelper = ocl.createOCLHelper();
-        
         oclHelper.setContext(Java_PackageImpl.eINSTANCE.getModel());
         try {
             expression = oclHelper.createQuery("ClassDeclaration.allInstances()->"
@@ -112,20 +189,18 @@ public class OclInterpreterBenchmark {
         } catch (ParserException e) {
             e.printStackTrace();
         }
-        this.query = ocl.createQuery(expression);
+        query = ocl.createQuery(expression);
         Object res = query.evaluate(resource.getContents().get(0));
-        time = System.currentTimeMillis()-time;
-        System.out.println("mdt-interpreter: invisibleMethods: "+time);
+        stopWatch();
+        System.out.println("results: "+((Collection)res).size());
 
 	}
 	
 	@Test
-	public void textElementInJavadocQuery(){
-		long time = System.currentTimeMillis();
+	public void textElementInJavadoc(){
+		System.out.println("textElementInJavadocQuery");
+		startWatch();
 		
-        this.ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
-        this.oclHelper = ocl.createOCLHelper();
-        
         oclHelper.setContext(Java_PackageImpl.eINSTANCE.getModel());
         try {
             expression = oclHelper.createQuery("self.compilationUnits.commentList->"
@@ -136,19 +211,17 @@ public class OclInterpreterBenchmark {
         } catch (ParserException e) {
             e.printStackTrace();
         }
-        this.query = ocl.createQuery(expression);
+        query = ocl.createQuery(expression);
         Object res = query.evaluate(resource.getContents().get(0));
-        time = System.currentTimeMillis()-time;
-        System.out.println("mdt-interpreter: textElementInJavadocQuery: "+time);
-
+        stopWatch();
+        System.out.println("results: "+((Collection)res).size());
 	}
 	
 	@Test
 	public void emptyTextElementInJavadoc (){
-		long time = System.currentTimeMillis();
+		System.out.println("emptyTextElementInJavadoc");
+		startWatch();
 		
-        this.ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
-        this.oclHelper = ocl.createOCLHelper();
         
         oclHelper.setContext(Java_PackageImpl.eINSTANCE.getModel());
         try {
@@ -161,37 +234,163 @@ public class OclInterpreterBenchmark {
         } catch (ParserException e) {
             e.printStackTrace();
         }
-        this.query = ocl.createQuery(expression);
+        query = ocl.createQuery(expression);
         Object res = query.evaluate(resource.getContents().get(0));
-        time = System.currentTimeMillis()-time;
-        System.out.println("mdt-interpreter: emptyTextElementInJavadoc : "+time);
-
+        stopWatch();
+        System.out.println("results: "+((Collection)res).size());
+        
 	}
+	
 	
 	@Test
 	public void grabats09(){
-		long time = System.currentTimeMillis();
+		System.out.println("grabats09");
+		startWatch();
 		
-        this.ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
-        this.oclHelper = ocl.createOCLHelper();
         
         oclHelper.setContext(Java_PackageImpl.eINSTANCE.getModel());
         try {
-            expression = oclHelper.createQuery("ClassDeclaration.allInstances()->"
-                    + "collect(bodyDeclarations)->"
-                    + "select(each | each.oclIsTypeOf(MethodDeclaration))->"
-                    + "select(each | (not each.modifier.oclIsUndefined()) "
-                    + "and (not each.modifier.visibility.oclIsUndefined()) "
-                    + "and (each.modifier.visibility = VisibilityKind::private "
-                    + "or each.modifier.visibility = VisibilityKind::protected))->"
+            expression = oclHelper.createQuery("TypeDeclaration.allInstances()->"
+                    + "select(each | each.bodyDeclarations->"
+                    + "exists(bd | bd.oclIsTypeOf(MethodDeclaration) "
+                    + "and (not bd.modifier.oclIsUndefined()) " + "and bd.modifier._static "
+                    + "and (not bd.oclAsType(MethodDeclaration).returnType.oclIsUndefined()) "
+                    + "and bd.oclAsType(MethodDeclaration).returnType.type = each))->"
                     + "asSequence()");
         } catch (ParserException e) {
             e.printStackTrace();
         }
-        this.query = ocl.createQuery(expression);
-        Object res = query.evaluate(resource.getContents().get(0));
+        query = ocl.createQuery(expression);
+        Object result = query.evaluate(resource.getContents().get(0));
+        stopWatch();
+			
+        System.out.println("results: "+((Collection)result).size());
+		
+	}
+	
+	
+	public void grabats09_(){
+		
+		Object res;
+		long time = System.currentTimeMillis();
+		
+	    OCLExpression<EClassifier> expression = null;
+	    Query<EClassifier, EClass, EObject> query = null;
+		
+        oclHelper.setContext(Java_PackageImpl.eINSTANCE.getModel());
+        
+        
+        
+        try {
+            expression = oclHelper.createQuery("TypeDeclaration.allInstances()");
+        } catch (ParserException e) {
+            e.printStackTrace();
+        }
+        query = ocl.createQuery(expression);
+        res = query.evaluate(resource.getContents().get(0));
         time = System.currentTimeMillis()-time;
         System.out.println("mdt-interpreter: grabats09: "+time);
+        System.out.println("mdt interpreter: grabats09 results: "+((Collection)res).size());        
+       
+        try {
+            expression = oclHelper.createQuery("TypeDeclaration.allInstances()->"
+                    + "select(each | each.bodyDeclarations->"
+                    + "exists(bd | bd.oclIsTypeOf(MethodDeclaration) "
+                    + "))");
+        } catch (ParserException e) {
+            e.printStackTrace();
+        }
+        query = ocl.createQuery(expression);
+        res = query.evaluate(resource.getContents().get(0));
+        time = System.currentTimeMillis()-time;
+        System.out.println("mdt-interpreter: grabats09: "+time);
+        System.out.println("mdt interpreter: grabats09 results: "+((Collection)res).size());
+        
+        
+        try {
+            expression = oclHelper.createQuery("TypeDeclaration.allInstances()->"
+                    + "select(each | each.bodyDeclarations->"
+                    + "exists(bd | bd.oclIsTypeOf(MethodDeclaration) "
+                    + "and (not bd.modifier.oclIsUndefined())))");
+        } catch (ParserException e) {
+            e.printStackTrace();
+        }
+        query = ocl.createQuery(expression);
+        res = query.evaluate(resource.getContents().get(0));
+        time = System.currentTimeMillis()-time;
+        System.out.println("mdt-interpreter: grabats09: "+time);
+        System.out.println("mdt interpreter: grabats09 results: "+((Collection)res).size());
+        
+        
+        try {
+            expression = oclHelper.createQuery("TypeDeclaration.allInstances()->"
+                    + "select(each | each.bodyDeclarations->"
+                    + "exists(bd | bd.oclIsTypeOf(MethodDeclaration) "
+                    + "and (not bd.modifier.oclIsUndefined()) " + "and bd.modifier._static))");
+        } catch (ParserException e) {
+            e.printStackTrace();
+        }
+        query = ocl.createQuery(expression);
+        res = query.evaluate(resource.getContents().get(0));
+        time = System.currentTimeMillis()-time;
+        System.out.println("mdt-interpreter: grabats09: "+time);
+        System.out.println("mdt interpreter: grabats09 results: "+((Collection)res).size());
+        
+        
+        try {
+            expression = oclHelper.createQuery("TypeDeclaration.allInstances()->"
+                    + "select(each | each.bodyDeclarations->"
+                    + "exists(bd | bd.oclIsTypeOf(MethodDeclaration) "
+                    + "and (not bd.modifier.oclIsUndefined()) " 
+                    + "and bd.modifier._static "
+                    + "and (not bd.oclAsType(MethodDeclaration).returnType.oclIsUndefined()) "
+                    + "))->"
+                    + "asSequence()");
+        } catch (ParserException e) {
+            e.printStackTrace();
+        }
+        query = ocl.createQuery(expression);
+        res = query.evaluate(resource.getContents().get(0));
+        time = System.currentTimeMillis()-time;
+        System.out.println("mdt-interpreter: grabats09: "+time);
+        System.out.println("mdt interpreter: grabats09 results: "+((Collection)res).size());
+	    
+	    
+        try {
+            expression = oclHelper.createQuery("TypeDeclaration.allInstances()->"
+                    + "select(each | each.bodyDeclarations->"
+                    + "exists(bd | bd.oclIsTypeOf(MethodDeclaration) "
+                    + "and (not bd.modifier.oclIsUndefined()) " + "and bd.modifier._static "
+                    + "and (not bd.oclAsType(MethodDeclaration).returnType.oclIsUndefined()) "
+                    + "and (not bd.oclAsType(MethodDeclaration).returnType.type.oclIsUndefined()) "
+                    + "and bd.oclAsType(MethodDeclaration).returnType.type = each))->"
+                    + "asSequence()");
+        } catch (ParserException e) {
+            e.printStackTrace();
+        }
+        query = ocl.createQuery(expression);
+        res = query.evaluate(resource.getContents().get(0));
+        time = System.currentTimeMillis()-time;
+        System.out.println("mdt-interpreter: grabats09: "+time);
+        System.out.println("mdt interpreter: grabats09 results: "+((Collection)res).size());
+        
+        
+        try {
+            expression = oclHelper.createQuery("TypeDeclaration.allInstances()->"
+                    + "select(each | each.bodyDeclarations->"
+                    + "exists(bd | bd.oclIsTypeOf(MethodDeclaration) "
+                    + "and (not bd.modifier.oclIsUndefined()) " + "and bd.modifier._static "
+                    + "and (not bd.oclAsType(MethodDeclaration).returnType.oclIsUndefined()) "
+                    + "and bd.oclAsType(MethodDeclaration).returnType.type = each))->"
+                    + "asSequence()");
+        } catch (ParserException e) {
+            e.printStackTrace();
+        }
+        query = ocl.createQuery(expression);
+        res = query.evaluate(resource.getContents().get(0));
+        time = System.currentTimeMillis()-time;
+        System.out.println("mdt-interpreter: grabats09: "+time);
+        System.out.println("mdt interpreter: grabats09 results: "+((Collection)res).size());
 	}	
 
 	
